7ae4ed3d4c25adb2728a58fed5ebcf4a1954dc42
beingakc <47496463+beingakc@users.noreply.github.com>
2019-03-07 11:14:51 +0545

    Create pointer.c

---
+++ pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a=100;
+	int *p,**p1;
+	p=&a;
+	
+	printf("%d\n",a); /*value of a */
+	printf("%p\n",&a);/*address of a */
+	printf("%d\n",*p);/*value of a */
+	printf("%d\n",p);/*value of p means address of a */
+	printf("%p\n",&p);/*address of p */
+	getch ();
+	return 0;
+}
